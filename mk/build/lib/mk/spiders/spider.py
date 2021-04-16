751b6db5f6b96534f8c0608c87f18729a2fd7872
Kevin Oh <cmyk.k4@gmail.com>
2021-04-16 03:11:59 -0400

    Update spider to fix size and price xpaths

--- mk/build/lib/mk/spiders/spider.py
+++ mk/build/lib/mk/spiders/spider.py
@@ -33,27 +33,39 @@ class BrandsSpider(scrapy.Spider):
 
     def parse_model(self, response):
         css = ".v3_specs tr:nth-child({row}) td:nth-child(2)::text"
+        findBasedOnText = "//*[contains(text(), '{text}')]/following::td/text()"
         # css = ".v3_specs tr:nth-child(1) td:nth-child(2)::text"
         # Target the specifications section for the juicy bits
         for info in response.css(".v3_specs"):
+            # yield {
+            #     "full_title": response.css(".header-detail .name::text").extract_first(),
+            #     "brand": info.css(css.format(row=1)).extract_first(),
+            #     "product_name": info.css(css.format(row=2)).extract_first(),
+            #     "size": info.css(css.format(row=3)).extract_first(),
+            #     "frame_color": info.css(css.format(row=7)).extract_first(),
+            #     "primary_led_color": info.css(css.format(row=10)).extract_first(),
+            #     "hotswappable": info.css(css.format(row=12)).extract_first(),
+            #     "interfaces": info.css(css.format(row=18)).extract_first(),
+            #     "windows_compatible": info.css(css.format(row=19)).extract_first(),
+            #     "mac_compatible": info.css(css.format(row=20)).extract_first(),
+            #     "dimensions": info.css(css.format(row=22)).extract_first(),
+            #     "weight": info.css(css.format(row=23)).extract_first(),
+            #     'price': info.css('#product_price::text').extract_first(),
+            # }
             yield {
                 "full_title": response.css(".header-detail .name::text").extract_first(),
+                "features": response.xpath("//h3[contains(text(), 'Features')]/following::ul[@class='acc_features']/*/text()").getall(),
                 "brand": info.css(css.format(row=1)).extract_first(),
                 "product_name": info.css(css.format(row=2)).extract_first(),
-                "size": info.css(css.format(row=3)).extract_first(),
-                "frame_color": info.css(css.format(row=7)).extract_first(),
-                "primary_led_color": info.css(css.format(row=10)).extract_first(),
-                "hotswappable": info.css(css.format(row=12)).extract_first(),
-                "interfaces": info.css(css.format(row=18)).extract_first(),
-                "windows_compatible": info.css(css.format(row=19)).extract_first(),
-                "mac_compatible": info.css(css.format(row=20)).extract_first(),
-                "dimensions": info.css(css.format(row=22)).extract_first(),
-                "weight": info.css(css.format(row=23)).extract_first(),
+                "size": info.xpath(findBasedOnText.format(text="Size")).extract_first(),
+                "frame_color": info.xpath(findBasedOnText.format(text="Frame Color")).extract_first(),
+                "primary_led_color": info.xpath(findBasedOnText.format(text="Primary LED Color")).extract_first(),
+                "hotswappable": info.xpath(findBasedOnText.format(text="Hotswap Sockets")).extract_first(),
+                "interfaces": info.xpath(findBasedOnText.format(text="Interface(s)")).extract_first(),
+                "windows_compatible": info.xpath(findBasedOnText.format(text="Windows Compatible")).extract_first(),
+                "mac_compatible": info.xpath(findBasedOnText.format(text="Mac Compatible")).extract_first(),
+                "linux_compatible": info.xpath(findBasedOnText.format(text="Linux Compatible")).extract_first(),
+                "dimensions": info.xpath(findBasedOnText.format(text="Dimensions")).extract_first(),
+                "weight": info.xpath(findBasedOnText.format(text="Weight")).extract_first(),
                 'price': info.css('#product_price::text').extract_first(),
-                # 'product_image': info.css('div.ph-product-img-ctn a').xpath('@href').extract(),
-                # 'sku': info.css('span.ph-pid').xpath('@prod-sku').extract_first(),
-                # 'short_description': info.css('div.ph-product-summary::text').extract_first(),
-                # 'price': info.css('h2.ph-product-price > span.price::text').extract_first(),
-                # 'long_description': info.css('div#product_tab_1').extract_first(),
-                # 'specs': info.css('div#product_tab_2').extract_first(),
             }
