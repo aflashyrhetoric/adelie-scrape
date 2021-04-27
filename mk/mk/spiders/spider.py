62ff6e1da8a4a39868041785f54a60328a31f853
Kevin Oh <cmyk.k4@gmail.com>
2021-04-26 23:41:07 -0400

    Update spider to include url and image path and description

--- mk/mk/spiders/spider.py
+++ mk/mk/spiders/spider.py
@@ -24,35 +24,17 @@ class BrandsSpider(scrapy.Spider):
             url = urljoin(response.url + "&show=100", p)
             yield scrapy.Request(url, callback=self.parse_model)
 
-    # def parse_product(self, response):
-    #     models = response.xpath("//div[contains(@class, 'product-name')]/a/@href").extract()
-    #     print("models\n" + json.dumps(models))
-    #     for m in models:
-    #         url = urljoin(response.url, m)
-    #         yield scrapy.Request(url, callback=self.parse_model)
-
     def parse_model(self, response):
         css = ".v3_specs tr:nth-child({row}) td:nth-child(2)::text"
         findBasedOnText = "//*[contains(text(), '{text}')]/following::td/text()"
+        # imagePathPrefix = "https://mechanicalkeyboards.com/shop/{text}"
         # css = ".v3_specs tr:nth-child(1) td:nth-child(2)::text"
         # Target the specifications section for the juicy bits
         for info in response.css(".v3_specs"):
-            # yield {
-            #     "full_title": response.css(".header-detail .name::text").extract_first(),
-            #     "brand": info.css(css.format(row=1)).extract_first(),
-            #     "product_name": info.css(css.format(row=2)).extract_first(),
-            #     "size": info.css(css.format(row=3)).extract_first(),
-            #     "frame_color": info.css(css.format(row=7)).extract_first(),
-            #     "primary_led_color": info.css(css.format(row=10)).extract_first(),
-            #     "hotswappable": info.css(css.format(row=12)).extract_first(),
-            #     "interfaces": info.css(css.format(row=18)).extract_first(),
-            #     "windows_compatible": info.css(css.format(row=19)).extract_first(),
-            #     "mac_compatible": info.css(css.format(row=20)).extract_first(),
-            #     "dimensions": info.css(css.format(row=22)).extract_first(),
-            #     "weight": info.css(css.format(row=23)).extract_first(),
-            #     'price': info.css('#product_price::text').extract_first(),
-            # }
             yield {
+                "url": response.url,
+                "img_path": response.css('.product-info img::attr(src)').extract_first(),
+                "product_description": response.css('.ldesc_fulldesc::text').extract_first(),
                 "full_title": response.css(".header-detail .name::text").extract_first(),
                 "features": response.xpath("//h3[contains(text(), 'Features')]/following::ul[@class='acc_features']/*/text()").getall(),
                 "brand": info.css(css.format(row=1)).extract_first(),
